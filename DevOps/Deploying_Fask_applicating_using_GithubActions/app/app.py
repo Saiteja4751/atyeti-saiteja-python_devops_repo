85472415925ef7016b09796b8b8222287f8f5dc9
Saiteja4751 <sai.battu@atyeti.com>
2025-07-07 12:55:41 +0530

    added files

--- DevOps/Deploying_Fask_applicating_using_GithubActions/app/app.py
+++ DevOps/Deploying_Fask_applicating_using_GithubActions/app/app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello from Flask in AWS ECR!"
+    return "Hello from ECS Flalkkkkskkslak App sai and co co workers the help of the local!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
